aa0fb062364e5367b6ab68466703a2faceb76bb6
Arman Dion Sakti <antonius.arman.jesuits@gmail.com>
2026-06-11 16:52:40 +0700

    Fix cache name typo in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "famili-finance-v1";
+const CACHE = "family-finance-v1";
 const STATIC = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", e => {
